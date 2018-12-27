8fabd1f09674cab275f599a1dbd941e91f61f6c2
wimbleclaw <aisha305@hotmail.co.uk>
2018-12-27 16:43:28 +0000

    moved map data into seperate component

---
+++ src/components/mapData.js
@@ -0,0 +1,208 @@
+const descriptionEngland = () => {
+    return '<a href="/Questionnaire">Compare your spending</a><br /><br />'
+}
+
+const mapData = () => {
+    return [
+        {
+            "id": "AU",
+            "value": 658189,
+        },
+        {
+            "id": "AT",
+            "value": 217653
+        },
+        {
+            "id": "BE",
+            "value": 255660
+        },
+        {
+            "id": "CA",
+            "value": 896977
+        },
+        {
+            "id": "CZ",
+            "value": 163205
+        },
+        {
+            "id": "DK",
+            "value": 126823
+        },
+        {
+            "id": "FI",
+            "value": 121446
+        },
+        {
+            "id": "FR",
+            "value": 1403614
+        },
+        {
+            "id": "DE",
+            "value": 2025195
+        },
+        {
+            "id": "GR",
+            "value": 193573
+        },
+        {
+            "id": "HU",
+            "value": 123185
+        },
+        {
+            "id": "IS",
+            "value": 7751
+        },
+        {
+            "id": "IE",
+            "value": 104897
+        },
+        {
+            "id": "IT",
+            "value": 1343002
+        },
+        {
+            "id": "JP",
+            "value": 2858279
+        },
+        {
+            "id": "KR",
+            "value": 843198
+        },
+        {
+            "id": "LU",
+            "value": 16924
+        },
+        {
+            "id": "MX",
+            "value": 1397196
+        },
+        {
+            "id": "NL",
+            "value": 374867
+        },
+        {
+            "id": "NZ",
+            "value": 98121
+        },
+        {
+            "id": "NO",
+            "value": 131627
+        },
+        {
+            "id": "PL",
+            "value": 582920
+        },
+        {
+            "id": "PT",
+            "value": 194040
+        },
+        {
+            "id": "SK",
+            "value": 86246
+        },
+        {
+            "id": "SE",
+            "value": 204506
+        },
+        {
+            "id": "CH",
+            "value": 272084
+        },
+        {
+            "id": "TR",
+            "value": 1173948
+        },
+        {
+            "id": "GB",
+            "value": 1690314,
+            "description": descriptionEngland()
+        },
+        {
+            "id": "US",
+            "value": 11927466
+        },
+        {
+            "id": "CN",
+            "value": 7647509
+        },
+        {
+            "id": "CO",
+            "value": 457024
+        },
+        {
+            "id": "EE",
+            "value": 718997
+        },
+        {
+            "id": "ID",
+            "value": 1604084
+        },
+        {
+            "id": "IL",
+            "value": 160787
+        },
+        {
+            "id": "RU",
+            "value": 1878152
+        },
+        {
+            "id": "SI",
+            "value": 34171
+        },
+        {
+            "id": "ZA",
+            "value": 434411
+        },
+        {
+            "id": "LV",
+            "value": 28685
+        },
+        {
+            "id": "BR",
+            "value": 2008772
+        },
+        {
+            "id": "LT",
+            "value": 52216
+        },
+        {
+            "id": "AR",
+            "value": 1591951
+        },
+        {
+            "id": "CR",
+            "value": 48779
+        },
+        {
+            "id": "BG",
+            "value": 80496
+        },
+        {
+            "id": "HR",
+            "value": 54175
+        },
+        {
+            "id": "CY",
+            "value": 18293
+        },
+        {
+            "id": "MT",
+            "value": 7540
+        },
+        {
+            "id": "RO",
+            "value": 259423
+        },
+        {
+            "id": "MK",
+            "value": 19497
+        },
+        {
+            "id": "ZM",
+            "value": 29721
+        },
+
+    ]
+}
+
+export default mapData
