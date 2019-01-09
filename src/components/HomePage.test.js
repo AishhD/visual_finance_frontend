d8839ac4733c35af905a8909ccc7d59a20fdc411
wimbleclaw <aisha305@hotmail.co.uk>
2019-01-09 16:56:05 +0000

    added test

--- src/components/HomePage.test.js
+++ src/components/HomePage.test.js
@@ -1,40 +1,25 @@
 import React from 'react'
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import Adapter from 'enzyme-adapter-react-16'
-import { mount, configure } from "enzyme";
+import { mount, shallow, configure } from "enzyme";
 import HomePage from './HomePage'
 
 configure({ adapter: new Adapter() });
 
-describe('Addition', () => {
-    it('knows that 2 and 2 make 4', () => {
-        expect(2 + 2).toBe(4);
-    });
-});
-
-describe("LockScreen", () => {
-    let props;
-    let mountedLockScreen;
-    const lockScreen = () => {
-        if (!mountedLockScreen) {
-            mountedLockScreen = mount(
-                <HomePage {...props} />
-            );
+describe("Home", () => {
+    let mountedHome;
+    const Home = () => {
+        if (!mountedHome) {
+            mountedHome = mount(
+                <Router>
+                    <HomePage />
+                </Router>);
         }
-        return mountedLockScreen;
+        return mountedHome;
     }
 
-    beforeEach(() => {
-        props = {
-            wallpaperPath: undefined,
-            userInfoMessage: undefined,
-            onUnlocked: undefined,
-        };
-        mountedLockScreen = undefined;
-    });
-
     it("always renders a div", () => {
-        const divs = lockScreen().find("div");
+        const divs = Home().find("div");
         expect(divs.length).toBeGreaterThan(0);
     });
 
